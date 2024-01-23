ad9493d8d5cb23c8c883cd0e527fd51d77d0220e
Siddharth Hiremath <siddharth.hiremath@uniphore.com>
2024-01-23 22:10:08 +0530

    role creation

---
+++ multi-tenant/environments/dev/clusters/in/eks-platform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+    cloud {
+      organization = "uniphore"
+      workspaces {
+        name = "dev"
+      }
+    }
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ab-south-1"
+}
+
+##########________________________IAM_ROLE________________#############
+
+data "aws_iam_policy" "eks_policy" {
+    name = "AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks-role" {
+    name = "eks-role"
+    assume_role_policy = data.aws_iam_policy.eks_policy.name
+}
