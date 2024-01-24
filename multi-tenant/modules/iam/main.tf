ccfcc19057a75082827e8909ed55e897513e032a
siddharth9611 <siddharthhiremath2054@gmail.com>
2024-01-24 20:45:32 +0530

    role creation

--- multi-tenant/modules/iam/main.tf
+++ multi-tenant/modules/iam/main.tf
@@ -1,8 +1,8 @@
 variable "name" {}
 variable "policy_arn" {}
 
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
+resource "aws_iam_role" "eks-role" {
+  name = var.name
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -21,6 +21,6 @@ resource "aws_iam_role" "test_role" {
 resource "aws_iam_policy_attachment" "eks_policy_attach" {
     name = var.name
     policy_arn = var.policy_arn
-    roles = [aws_iam_role.test_role.name]
+    roles = [aws_iam_role.eks-role.name]
 
 }
