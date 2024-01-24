457b74847f4a63bba715c57bdbbd921e925c05d4
siddharth9611 <siddharthhiremath2054@gmail.com>
2024-01-24 20:42:53 +0530

    role creation

--- multi-tenant/modules/iam/main.tf
+++ multi-tenant/modules/iam/main.tf
@@ -1,29 +1,26 @@
 variable "name" {}
 variable "policy_arn" {}
 
-
-
-
-
-
-resource "aws_iam_role" "eks-role" {
-    name = var.name
-    assume_role_policy ={
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "eks.amazonaws.com"
-                }
-                "Action": "sts:AssumeRole"
-            }
-        ]
-            
-    }
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 resource "aws_iam_policy_attachment" "eks_policy_attach" {
     name = var.name
     policy_arn = var.policy_arn
+    roles = [aws_iam_role.test_role.name]
+
 }
