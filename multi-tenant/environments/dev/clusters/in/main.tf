0083fcb2d835b4311c1b73e79e1a49b916d5d24a
Siddharth Hiremath <siddharthhiremath2054@gmail.com>
2024-12-18 21:37:20 +0530

    disable nat

--- multi-tenant/environments/dev/clusters/in/main.tf
+++ multi-tenant/environments/dev/clusters/in/main.tf
@@ -26,6 +26,7 @@ module "main_vpc" {
   source             = "../../../../modules/vpc_V2"
   name               = "devops-practise"
   availability_zones = ["ap-south-1a", "ap-south-1c", "ap-south-1b"]
+  enable_nat_gateway = false
 }
 
 #######--------------outputs-------------##############
