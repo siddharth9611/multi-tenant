db84bf71801454b7dc939a98497a1ac5d3dd9c63
Siddharth Hiremath <siddharthhiremath2054@gmail.com>
2024-12-18 21:28:49 +0530

    changes

--- multi-tenant/modules/vpc_V2/main.tf
+++ multi-tenant/modules/vpc_V2/main.tf
@@ -16,8 +16,8 @@ variable "cidr" {
   default = "10.0.0.0/16"
 }
 variable "private_subnets" {
-  type = list(string)
-  default =  ["10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"]
+  type    = list(string)
+  default = ["10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"]
 }
 variable "public_subnets" {
   default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -44,18 +44,20 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~>3.0"
 
-  name                      = var.name
-  cidr                      = var.cidr
-  azs                       = var.availability_zones
-  private_subnets           = var.private_subnets
-  public_subnets            = var.public_subnets
-  enable_nat_gateway        = true
-  single_nat_gateway        = true
-  enable_dns_hostnames      = false
-  enable_flow_log           = false
-  tags                = local.vpc_tags
-  public_subnet_tags  = local.public_subnet_tags
-  private_subnet_tags = local.private_subnet_tags
+  name                           = var.name
+  cidr                           = var.cidr
+  azs                            = var.availability_zones
+  private_subnets                = var.private_subnets
+  public_subnets                 = var.public_subnets
+  enable_nat_gateway             = true
+  single_nat_gateway             = true
+  enable_dns_hostnames           = false
+  one_nat_gateway_per_az         = false
+  enable_flow_log                = false
+  default_vpc_enable_classiclink = false
+  tags                           = local.vpc_tags
+  public_subnet_tags             = local.public_subnet_tags
+  private_subnet_tags            = local.private_subnet_tags
 
 }
 
